a18ab8861c7dfbd816ef3aab91f80b469d585569
pankajmulik <pankajmulik1997@gmail.com>
2024-02-12 16:28:07 +0530

    dashboard

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import menus from './sidebarmenu'
 import SidebarContent from './SidebarContent'
 import {Col,Container,Row} from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 
 const Sidebar = ({ children }) => {
 
+    const [isOpen,setisopen]=useState("true")
+
     return (
         
 
-            <Container  >
-              
-                <Row className="justify-content-md-left">
-                    <Col className='xs' lg="2">
-                        1 of 3
-                    </Col>
-                  
-                
-                </Row>
+        <div className='container' style={{margin:'0'}}>
+            <div className='row d-flex '>
+                <div className='col-2  d.flex' style={isOpen?{width:'200px'}:{width:'0px'}}>
+                   
+                   
+                   
+                    <img src="images/numetry_technologies_logo.jpg" alt="no found" className='img-thumbnail' style={{ width: '50px', height: '50px', objectFit: 'fill' }} />
 
-</Container>
+               
+                    
+                </div>
+                <div style={{backgroundColor:"blue"}}> <FontAwesomeIcon icon={faBars} /></div>
+            
+            </div>
+      </div>
 
 
 
